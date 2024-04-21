83570fa29f14703d87fd45f10078621d298e689a
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-04-21 17:22:39 +0500

    Update MoviesCard.jsx

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,8 +1,91 @@
+// import './MoviesCard.css'
+
+// import { useContext, useState, useEffect } from 'react'
+// import { useLocation } from 'react-router-dom'
+
+// import { MoviesContext } from './../../contexts/MoviesContext'
+
+// export default function MoviesCard({ movie }) {
+//   const { savedMoviesList, saveMovie, deleteMovie } = useContext(MoviesContext)
+
+//   const { duration, image: imageURL, nameRU, trailerLink } = movie
+
+//   const isLiked = savedMoviesList.some((m) => m.id === movie.id)
+
+//   const [isMovieSaved, setIsMovieSaved] = useState(isLiked)
+
+//   const likeButtonClassName = `movies-card__button ${isMovieSaved ? 'movies-card__button_selected' : 'movies-card__button_unselected'}`
+
+//   useEffect(() => {
+//     const savedMoviesIds = JSON.parse(localStorage.getItem('savedMovies')) || []
+//     setIsMovieSaved(savedMoviesIds.includes(movie.movieId))
+//   }, [movie.movieId])
+
+//   const location = useLocation()
+
+//   const timeConvertor = (m) => {
+//     return `${Math.floor(m / 60)}ч ${m % 60}м`
+//   }
+
+//   const handleToggleMovie = () => {
+//     if (!isMovieSaved) {
+//       saveMovie(movie)
+//         .then(() => {
+//           setIsMovieSaved(true)
+//           const savedMoviesIds = JSON.parse(localStorage.getItem('savedMovies')) || []
+//           localStorage.setItem('savedMovies', JSON.stringify([...savedMoviesIds, movie.id]))
+//         })
+//         .catch((err) => {
+//           console.error(err)
+//         })
+//     } else {
+//       deleteMovie(movie.movieId)
+//         .then(() => {
+//           const savedMoviesIds = JSON.parse(localStorage.getItem('savedMovies')) || []
+//           const updatedMovieIds = savedMoviesIds.filter((id) => id !== movie.id)
+//           setIsMovieSaved(false)
+//           localStorage.setItem('savedMovies', JSON.stringify(updatedMovieIds))
+//         })
+//         .catch((err) => {
+//           console.error(err)
+//         })
+//     }
+//   }
+
+//   return (
+//     <li className="movies-card">
+//       <a
+//         href={trailerLink}
+//         className="movies-card__link"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         aria-label={`Трейлер к фильму ${nameRU}`}
+//       >
+//         <img src={imageURL} alt="обложка кина" className="movies-card__image" />
+//       </a>
+//       <div className="movies-card__container">
+//         <h1 className="movies-card__title">{nameRU}</h1>
+//         <p className="movies-card__time">{timeConvertor(duration)}</p>
+//       </div>
+
+//       {location.pathname === '/saved-movies' ? (
+//         <button
+//           className="movies-card__button movies-card__button_cross"
+//           onClick={handleToggleMovie}
+//         ></button>
+//       ) : (
+//         <button className={likeButtonClassName} onClick={handleToggleMovie}>
+//           Сохранить
+//         </button>
+//       )}
+//     </li>
+//   )
+// }
+
 import './MoviesCard.css'
 
 import { useContext, useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-
 import { MoviesContext } from './../../contexts/MoviesContext'
 
 export default function MoviesCard({ movie }) {
@@ -10,18 +93,16 @@ export default function MoviesCard({ movie }) {
 
   const { duration, image: imageURL, nameRU, trailerLink } = movie
 
-  const isLiked = savedMoviesList.some((m) => m.id === movie.id)
-
-  const [isMovieSaved, setIsMovieSaved] = useState(isLiked)
-
-  const likeButtonClassName = `movies-card__button ${isMovieSaved ? 'movies-card__button_selected' : 'movies-card__button_unselected'}`
-
-  useEffect(() => {
+  const [isMovieSaved, setIsMovieSaved] = useState(() => {
     const savedMoviesIds = JSON.parse(localStorage.getItem('savedMovies')) || []
-    setIsMovieSaved(savedMoviesIds.includes(movie.movieId))
-  }, [movie.movieId])
+    return savedMoviesIds.includes(movie.movieId)
+  })
 
   const location = useLocation()
+
+  useEffect(() => {
+    setIsMovieSaved(savedMoviesList.some((m) => m.id === movie.id))
+  }, [savedMoviesList, movie.id])
 
   const timeConvertor = (m) => {
     return `${Math.floor(m / 60)}ч ${m % 60}м`
@@ -52,6 +133,8 @@ export default function MoviesCard({ movie }) {
     }
   }
 
+  const likeButtonClassName = `movies-card__button ${isMovieSaved ? 'movies-card__button_selected' : 'movies-card__button_unselected'}`
+
   return (
     <li className="movies-card">
       <a
@@ -75,7 +158,7 @@ export default function MoviesCard({ movie }) {
         ></button>
       ) : (
         <button className={likeButtonClassName} onClick={handleToggleMovie}>
-          Сохранить
+          {isMovieSaved ? 'Удалить' : 'Сохранить'}
         </button>
       )}
     </li>
