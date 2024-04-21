e6e1a3d8ddc7bce403797cb0cca9bb68969a3b31
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-04-22 01:30:26 +0500

    Update MoviesCard.jsx

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -339,25 +339,101 @@
 //   )
 // }
 
+// import './MoviesCard.css'
+// import { useContext, useState, useEffect } from 'react'
+// import { useLocation } from 'react-router-dom'
+// import { MoviesContext } from './../../contexts/MoviesContext'
+
+// export default function MoviesCard({ movie }) {
+//   const { saveMovie, deleteMovie } = useContext(MoviesContext)
+
+//   const { duration, image: imageURL, nameRU, trailerLink } = movie
+
+//   const [isMovieSaved, setIsMovieSaved] = useState(false)
+
+//   useEffect(() => {
+//     const savedMoviesFromStorage = sessionStorage.getItem('savedMovies')
+//       ? JSON.parse(sessionStorage.getItem('savedMovies'))
+//       : []
+//     const isLiked = savedMoviesFromStorage.some((m) => m.movieId === movie.movieId)
+//     setIsMovieSaved(isLiked)
+//   }, [movie.movieId])
+
+//   const location = useLocation()
+
+//   const timeConvertor = (m) => {
+//     return `${Math.floor(m / 60)}ч ${m % 60}м`
+//   }
+
+//   const handleToggleMovie = () => {
+//     const savedMoviesFromStorage = sessionStorage.getItem('savedMovies')
+//       ? JSON.parse(sessionStorage.getItem('savedMovies'))
+//       : []
+
+//     if (!isMovieSaved) {
+//       saveMovie(movie)
+//         .then(() => {
+//           const updatedSavedMoviesList = [...savedMoviesFromStorage, movie]
+//           sessionStorage.setItem('savedMovies', JSON.stringify(updatedSavedMoviesList))
+//           setIsMovieSaved(true)
+//         })
+//         .catch((err) => console.log(err))
+//     } else {
+//       deleteMovie(movie.movieId)
+//         .then(() => {
+//           const updatedSavedMoviesList = savedMoviesFromStorage.filter((m) => m.movieId !== movie.movieId)
+//           sessionStorage.setItem('savedMovies', JSON.stringify(updatedSavedMoviesList))
+//           setIsMovieSaved(false)
+//         })
+//         .catch((err) => console.log(err))
+//     }
+//   }
+
+//   return (
+//     <li className="movies-card">
+//       <a
+//         href={trailerLink}
+//         className="movies-card__link"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         aria-label={`Трейлер к фильму ${nameRU}`}
+//       >
+//         <img src={imageURL} alt="обложка кина" className="movies-card__image" />
+//       </a>
+//       <div className="movies-card__container">
+//         <h1 className="movies-card__title">{nameRU}</h1>
+//         <p className="movies-card__time">{timeConvertor(duration)}</p>
+//       </div>
+
+//       {location.pathname === '/saved-movies' ? (
+//         <button
+//           className="movies-card__button movies-card__button_cross"
+//           onClick={handleToggleMovie}
+//         ></button>
+//       ) : (
+//         <button
+//           className={`movies-card__button ${isMovieSaved ? 'movies-card__button_selected' : 'movies-card__button_unselected'}`}
+//           onClick={handleToggleMovie}
+//         >
+//           {isMovieSaved ? 'Удалить' : 'Сохранить'}
+//         </button>
+//       )}
+//     </li>
+//   )
+// }
+
+
 import './MoviesCard.css'
-import { useContext, useState, useEffect } from 'react'
+import { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import { MoviesContext } from './../../contexts/MoviesContext'
 
 export default function MoviesCard({ movie }) {
-  const { saveMovie, deleteMovie } = useContext(MoviesContext)
+  const { savedMoviesList, saveMovie, deleteMovie } = useContext(MoviesContext)
 
-  const { duration, image: imageURL, nameRU, trailerLink } = movie
+  const { duration, image: imageURL, nameRU, trailerLink, movieId } = movie
 
-  const [isMovieSaved, setIsMovieSaved] = useState(false)
-
-  useEffect(() => {
-    const savedMoviesFromStorage = sessionStorage.getItem('savedMovies')
-      ? JSON.parse(sessionStorage.getItem('savedMovies'))
-      : []
-    const isLiked = savedMoviesFromStorage.some((m) => m.movieId === movie.movieId)
-    setIsMovieSaved(isLiked)
-  }, [movie.movieId])
+  const isMovieSaved = savedMoviesList.some((m) => m.movieId === movieId)
 
   const location = useLocation()
 
@@ -375,15 +451,13 @@ export default function MoviesCard({ movie }) {
         .then(() => {
           const updatedSavedMoviesList = [...savedMoviesFromStorage, movie]
           sessionStorage.setItem('savedMovies', JSON.stringify(updatedSavedMoviesList))
-          setIsMovieSaved(true)
         })
         .catch((err) => console.log(err))
     } else {
-      deleteMovie(movie.movieId)
+      deleteMovie(movieId)
         .then(() => {
-          const updatedSavedMoviesList = savedMoviesFromStorage.filter((m) => m.movieId !== movie.movieId)
+          const updatedSavedMoviesList = savedMoviesFromStorage.filter((m) => m.movieId !== movieId)
           sessionStorage.setItem('savedMovies', JSON.stringify(updatedSavedMoviesList))
-          setIsMovieSaved(false)
         })
         .catch((err) => console.log(err))
     }
